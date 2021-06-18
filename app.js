33fc67b71ea58cdca255cf3f8c5def5d6e539be5
Scott Severance <scott@scottseverance.us>
2021-06-18 13:44:30 -0500

    exercise complete

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(let i=1; i<=100; i++) {
+    if(i % 2 == 1) {
+        console.log(i);
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function fizzbuzz(n) {
+    out = '';
+    if(n % 3 == 0) {
+        out = 'FIZZ';
+    }
+    if(n % 5 == 0) {
+        out += 'BUZZ';
+    }
+    return out;
+}
+for(let i=1; i<=100; i++) {
+    console.log(`${i}: ${fizzbuzz(i)}`)
+}
