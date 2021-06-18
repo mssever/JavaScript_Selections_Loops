c0b026d7aef2fa2df88b9a0158a674790b3d0743
Scott Severance <scott@scottseverance.us>
2021-06-18 14:17:31 -0500

    bugfix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ for(let i=1; i<=100; i++) {
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 function fizzbuzz(n) {
-    out = '';
+    let out = '';
     if(n % 3 == 0) {
         out = 'FIZZ';
     }
