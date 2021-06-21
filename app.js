a16bbaa674bbd6a9034241ca2d4f8d9c18755e45
Scott Severance <scott@scottseverance.us>
2021-06-21 11:55:58 -0500

    refactored skip even

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-for(let i=1; i<=100; i++) {
-    if(i % 2 == 1) {
-        console.log(i);
-    }
-}
+for(let i=1; i<=100; i += 2) console.log(i);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
